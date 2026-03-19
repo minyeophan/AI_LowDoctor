75633d01a76d77d579250b232f4cebfbe6203b7f
seoee04 <leeseoyun04@gmail.com>
2026-03-19 20:33:23 +0900

    feat: 댓글 수정 기능 구현

--- backend/src/controllers/comment_controller.js
+++ backend/src/controllers/comment_controller.js
@@ -14,3 +14,27 @@ export const uploadComment = async (req, res, next) => {
         next(error);
     }
 };
+
+export const updateComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.id);
+
+        if (!comment) {
+            return res.status(404).json({ message: "댓글 없음" });
+        }
+
+        if (comment.userID.toString() !== req.user.id) {
+            return res.status(403).json({ message: "권한 없음" });
+        }
+
+        if (req.body.comment !== undefined) {
+            comment.comment = req.body.comment;
+        }
+
+        await comment.save();
+        res.json(comment);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+};
--- backend/src/routes/comment_routes.js
+++ backend/src/routes/comment_routes.js
@@ -3,5 +3,6 @@ import express from "express";
 const router = express.Router();
 
 router.post('/', uploadComment);
+router.patch('/:id', updateComment);
 
 export default router;
