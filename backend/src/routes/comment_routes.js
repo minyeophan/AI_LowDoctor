17bcda7c1fc14fe13fde62d5196e004c5d4274cd
지우 <u@jiuui-MacBookAir.local>
2026-03-21 12:44:50 +0900

    chore: AI, backend 폴더 커밋에서 제거

--- backend/src/routes/comment_routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.post('/', uploadComment);
-router.patch('/:id', updateComment);
-router.delete('/:id', deleteComment);
-
-export default router;
