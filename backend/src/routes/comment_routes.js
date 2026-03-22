1c3f462a7f54aa87cc4156562c2c8cec1990d332
seoee04 <leeseoyun04@gmail.com>
2026-03-22 22:21:06 +0900

    feat: auth 미들웨어 comment_routes.js에 연결

--- backend/src/routes/comment_routes.js
+++ backend/src/routes/comment_routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 
 import { uploadComment, updateComment, deleteComment, likeComment, categoryComment } from "../controllers/comment_controller";
+import isLoggedIn from "../middleware/auth_middle";
 
 const router = express.Router();
 
-router.post('/', uploadComment);
-router.patch('/:id', updateComment);
-router.delete('/:id', deleteComment);
-router.post('/:id/like', likeComment);
+router.post('/', isLoggedIn, uploadComment);
+router.patch('/:id', isLoggedIn, updateComment);
+router.delete('/:id', isLoggedIn, deleteComment);
+router.post('/:id/like', isLoggedIn, likeComment);
 router.get('/', categoryComment);
 
 export default router;
