5990d940a03a86e261d002cc8c1f9af3a28095f3
seoee04 <leeseoyun04@gmail.com>
2026-03-22 21:47:23 +0900

    feat: auth 미들웨어 post_routes.js에 연결

--- backend/src/routes/post_routes.js
+++ backend/src/routes/post_routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 
 import { uploadPost, updatePost, deletePost, viewsPost, categoryPost, likePost } from "../controllers/post_controller.js";
+import isLoggedIn from "../middleware/auth_middle.js";
 
 const router = express.Router();
 
-router.post('/', uploadPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.post('/:id/like', likePost);
+router.post('/', isLoggedIn, uploadPost);
+router.patch('/:id', isLoggedIn, updatePost);
+router.delete('/:id', isLoggedIn, deletePost);
+router.post('/:id/like', isLoggedIn, likePost);
 router.get('/', categoryPost);
 router.get('/:id', viewsPost);
 
