a03205f0565997b719b156d268313abb400005fd
seoee04 <leeseoyun04@gmail.com>
2026-03-28 19:29:16 +0900

    feat: 마이페이지 auth 미들웨어 적용

--- backend/src/routes/mypage_routes.js
+++ backend/src/routes/mypage_routes.js
@@ -1,8 +1,9 @@
 import express from "express";
+import authMiddleware from "../middleware/auth_middle.js";
 import { getMyPageList } from "../controllers/mypage_controller.js";
 
 const router = express.Router();
 
-router.get('/', getMyPageList);
+router.get('/', authMiddleware, getMyPageList);
 
 export default router;
