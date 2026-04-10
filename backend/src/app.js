e06a82afcd0147e393d2231f82dd2d981ccd321b
seoee04 <leeseoyun04@gmail.com>
2026-04-10 17:14:44 +0900

    feat: 핵심 요약 JSON 형식으로 수정 및 용량 제한 늘림

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,8 +43,8 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
